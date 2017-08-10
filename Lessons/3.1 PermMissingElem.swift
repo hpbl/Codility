58a7505b2d8053748814d8e5eec190e2f9befd80
Hilton Pintor <hiltonpintor@gmail.com>
2017-08-10 11:27:15 -0300

    Create 3.1 PermMissingElem.swift

---
+++ Lessons/3.1 PermMissingElem.swift
@@ -0,0 +1,52 @@
+/*
+A zero-indexed array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+public func solution(_ A : inout [Int]) -> Int
+
+that, given a zero-indexed array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Assume that:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+
+Copyright 2009–2017 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+*/
+
+//RESULT: 100% CORRECTNESS, 100% PERFORMANCE
+
+import Foundation
+import Glibc
+
+// you can write to stdout for debugging purposes, e.g.
+// print("this is a debug message")
+
+public func solution(_ A : inout [Int]) -> Int {
+    // write your code in Swift 3.0 (Linux)
+    var aux = Array(repeating: -1, count: A.count + 1)
+
+
+    for element in A {
+        aux[element-1] = element
+    }
+
+    return aux.index(of: -1)! + 1
+}
