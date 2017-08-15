e0470490cee6c5be212ea179b352027ab9c09b29
Hilton Pintor <hiltonpintor@gmail.com>
2017-08-14 21:40:56 -0300

    Create 4.1 MissingInteger.swift

---
+++ Lessons/4.1 MissingInteger.swift
@@ -0,0 +1,52 @@
+/*
+Write a function:
+
+public func solution(_ A : inout [Int]) -> Int
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+For another example, given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Assume that:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+
+Copyright 2009–2017 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+*/
+
+//RESULT: 100% CORRECTNESS, 100% PERFORMANCE
+
+import Foundation
+import Glibc
+
+// you can write to stdout for debugging purposes, e.g.
+// print("this is a debug message")
+
+public func solution(_ A : inout [Int]) -> Int {
+    // write your code in Swift 3.0 (Linux)
+    var occur: [Int : Int] = [:]
+    var min: Int = -1
+
+    for element in A {
+        occur[element] = 1
+    }
+
+    for integer in 1...1000000 {
+        if occur[integer] == nil {
+            min =  integer
+            break
+        }
+    }
+    
+    return min
+}
